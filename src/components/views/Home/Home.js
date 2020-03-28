39b4290a8cab2e4115de559e8ec19198d868d856
sinsys <nicholas.hazel@gmail.com>
2020-03-28 09:41:38 -0700

    Adding coloring and styling

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -20,19 +20,45 @@ const Landing = () => {
         <span className="maze-highlight">MAZE</span>
         ment
       </div>
-      <div className="btn-options">
-        
-      </div>
-      <Button
-        id="launch-btn"
-        className="btn draw-border"
-        type="button"
-        name="launch-btn"
-        text="Launch"
-        onClick={() => {
-          console.log('clicked');
-        }}
-      />
+
+      <section className="btn-options_wrapper">
+        <div className="btn-option">
+          <Button
+            id="launch-btn"
+            className="launch-btn draw-border-launch btn"
+            type="button"
+            name="launch-btn"
+            text="Launch"
+            onClick={() => {
+              console.log('clicked');
+            }}
+          />
+        </div>
+        <div className="btn-options-row">
+          <Button
+              id="login-btn"
+              className="login-btn draw-border-login btn"
+              type="button"
+              name="login-btn"
+              text="Log In"
+              onClick={() => {
+                console.log('clicked');
+              }}
+            />    
+          <Button
+            id="signup-btn"
+            className="signup-btn draw-border-signup btn"
+            type="button"
+            name="signup-btn"
+            text="Sign Up"
+            onClick={() => {
+              console.log('clicked');
+            }}
+          />
+        </div>
+      </section>
+
+
     </div>
   );
 
---
+++ src/components/views/Home/Home.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { MazeContextProvider } from 'contexts/MazeContext';
+import Home from './Home';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+  	<Router>
+      <MazeContextProvider>
+  		  <Home />
+      </MazeContextProvider>
+  	</Router>,
+  	div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
